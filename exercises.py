ed46bedf9b8b300fc1e20d216b48f4d84eea520d
0Teken6 <timu.rev006@gmail.com>
2024-06-11 00:09:30 +0600

    text editor

--- exercises.py
+++ exercises.py
@@ -1,46 +1,48 @@
 import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, mode="w", encoding="utf-8") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"Simple Text Editor - {filepath}")
 
 window = tk.Tk()
-window.title("Address Entry Form")
+window.title("Text Editor")
 
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
 
-frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
+txt_edit = tk.Text(window)
+frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
+btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
 
-frm_form.pack()
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 
-
-labels = [
-    "First Name:",
-    "Last Name:",
-    "Address Line 1:",
-    "Address Line 2:",
-    "City:",
-    "State/Province:",
-    "Postal Code:",
-    "Country:",
-]
-
-
-for idx, text in enumerate(labels):
-
-    label = tk.Label(master=frm_form, text=text)
-
-    entry = tk.Entry(master=frm_form, width=50)
-
-    label.grid(row=idx, column=0, sticky="e")
-    entry.grid(row=idx, column=1)
-
-
-frm_buttons = tk.Frame()
-frm_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
-
-
-btn_submit = tk.Button(master=frm_buttons, text="Submit")
-btn_submit.pack(side=tk.RIGHT, padx=10, ipadx=10)
-
-
-btn_clear = tk.Button(master=frm_buttons, text="Clear")
-btn_clear.pack(side=tk.RIGHT, ipadx=10)
+frm_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
 
 window.mainloop()
